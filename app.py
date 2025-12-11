dd16ae42d9f48ffaa47ec9cf21aad93c65f0f66e
winterlyembrace <4icken00@gmail.com>
2025-12-11 22:45:14 +1000

    Add files via upload
    Updated app.py to use dynamic PORT for cloud deployment.

--- app.py
+++ app.py
@@ -37,3 +37,10 @@ def home():
 if __name__ == '__main__':
     
     app.run(host='0.0.0.0', port=5000)
+
+import os
+
+if __name__ == '__main__':
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
